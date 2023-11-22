237469c7e19f363b1b2282e0caa8700a4292da61
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-11-22 18:48:53 +0100

    Initial Commit

---
+++ Getting data from API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Getting data from API
+//
+//  Created by Macbook Air 2017 on 22. 11. 2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
